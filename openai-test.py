12f8a68d7c1dc70f25e22cdcb299adb14aee5dcf
afwill45 <144282281+afwill45@users.noreply.github.com>
2024-06-10 18:20:25 -0400

    Added more functionality to parser

--- openai-test.py
+++ openai-test.py
@@ -4,17 +4,33 @@ import argparse
 import os
 import re
 
+model_use = "gpt-3.5-turbo"
+platform = "OpenAI"
+model_choices = ["gpt-3.5-turbo", "3.5", "gpt-4o", "4o", "gpt-4-turbo", "4"]
+
+
 parser = argparse.ArgumentParser()
 parser.parse_args()
-parser.add_argument("-m", "--model", help ==)
+parser.add_argument("-m", "--model", help="The OpenAI model", choices = model_choices)
+args = parser.parse_args()
+if platform == "OpenAI":
+  if (args.model == "gpt-4o") or (args.model == "4o"):
+    model_use = "gpt-4o"
+  elif(args.model == "gpt-4-turbo") or (args.model == "4"):
+    model_use = "gpt-4-turbo"
+'''
+When integration for other platforms like Anthrophic's Claude and Google Gemini
+Increase the model choices to account for them as well
+And create paramters for platform
+'''
+    
+    
 
 
 
-client = OpenAI(organization='org-dNPZULtlZEweJKpXsK80byiq')
-
-
+client = OpenAI()
 completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
+  model= model_use
   messages=[
     {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
     {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
